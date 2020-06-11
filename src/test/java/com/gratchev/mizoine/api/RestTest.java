7ae5978ef7778adc693b504686c156f69bdfe1f3
Artem <artem@gratchev.com>
2020-06-11 17:39:42 +0200

    Pretty print JSON response body

--- src/test/java/com/gratchev/mizoine/api/RestTest.java
+++ src/test/java/com/gratchev/mizoine/api/RestTest.java
@@ -13,15 +13,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultHandler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = { "users.amadeus.password=god" })
 @AutoConfigureMockMvc
 public class RestTest {
+	private static final Logger log = LoggerFactory.getLogger(RestTest.class);
 
 	@Autowired
 	private MockMvc mvc;
@@ -50,7 +57,17 @@ public class RestTest {
 
 	@Test
 	public void shouldReadAppInfo() throws Exception {
-		mvc.perform(get("/api/app").with(user("hacker"))).andDo(print()).andExpect(status().isOk());
+		mvc.perform(get("/api/app").with(user("hacker"))).andDo(prettyPrintJson()).andExpect(status().isOk());
+	}
+
+	public static ResultHandler prettyPrintJson() {
+		return r -> {
+			log.info("Status: {}", r.getResponse().getStatus());
+			final ObjectMapper mapper = new ObjectMapper();
+			final Object jsonObject = mapper.readValue(r.getResponse().getContentAsString(), Object.class);
+			final String prettyJson = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+			log.info("Body: {}", prettyJson);
+		};
 	}
 
 	@Test
