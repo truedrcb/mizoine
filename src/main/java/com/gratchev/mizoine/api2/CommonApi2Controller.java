1eb10202d2245b9a01ad2296bdb2d740a7af9ab8
Artem <artem@gratchev.com>
2020-11-11 23:33:05 +0100

    Sample Swagger summary for springdoc OpenAPI

--- src/main/java/com/gratchev/mizoine/api2/CommonApi2Controller.java
+++ src/main/java/com/gratchev/mizoine/api2/CommonApi2Controller.java
@@ -1,12 +1,19 @@
 package com.gratchev.mizoine.api2;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gratchev.mizoine.api.ProjectApiController.ProjectInfo;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 @Controller
 @RequestMapping("/api2")
@@ -29,4 +36,10 @@ public class CommonApi2Controller {
 		return appInfo;
 	}
 
+	@Operation(summary = "Retrieve projects list")
+	@GetMapping("/repositories({repositoryId})/projects")
+	@ResponseBody
+	public List<ProjectInfo> getProjects(@PathVariable final String repositoryId) {
+		return List.of();
+	}
 }
