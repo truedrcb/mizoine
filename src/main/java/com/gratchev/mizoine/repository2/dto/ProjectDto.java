707020a56d9ed764dea943a4ea5d057e0a983726
Artem <artem@gratchev.com>
2020-11-13 23:21:15 +0100

    New repository admin interface implementation
    - Create project
    - Get list of projects

---
+++ src/main/java/com/gratchev/mizoine/repository2/dto/ProjectDto.java
@@ -0,0 +1,31 @@
+package com.gratchev.mizoine.repository2.dto;
+
+public class ProjectDto {
+	private String projectId;
+	private String description;
+	private String descriptionHtml;
+
+	public String getProjectId() {
+		return projectId;
+	}
+
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDescriptionHtml() {
+		return descriptionHtml;
+	}
+
+	public void setDescriptionHtml(String descriptionHtml) {
+		this.descriptionHtml = descriptionHtml;
+	}
+}
---
+++ src/main/java/com/gratchev/mizoine/repository2/file/AdministratorImpl.java
@@ -0,0 +1,53 @@
+package com.gratchev.mizoine.repository2.file;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.gratchev.mizoine.repository2.Administrator;
+import com.gratchev.mizoine.repository2.dto.ProjectDto;
+
+
+/**
+ * File system based {@link Administrator} implementation
+ * 
+ * @author artem@gratchev.com
+ */
+public class AdministratorImpl implements Administrator {
+	private static final Logger log = LoggerFactory.getLogger(AdministratorImpl.class);
+	private final Path rootPath;
+
+	public AdministratorImpl(final Path rootPath) {
+		this.rootPath = rootPath;
+	}
+
+	public Path getRootPath() {
+		return rootPath;
+	}
+
+	@Override
+	public void createProject(final String projectId) throws IOException {
+		log.debug("Creating project: {}", projectId);
+		final Path projectPath = rootPath.resolve(Path.of(projectId));
+		Files.createDirectories(projectPath);
+		log.info("New project has been created in folder: {}", projectPath);
+	}
+
+	@Override
+	public List<ProjectDto> getProjects() {
+		final ArrayList<ProjectDto> repositoriesIds = new ArrayList<>();
+		for (final var file : rootPath.toFile().listFiles()) {
+			if (file.isDirectory() && !file.isHidden()) {
+				final ProjectDto dto = new ProjectDto();
+				dto.setProjectId(file.getName());
+				repositoriesIds.add(dto);
+			}
+		}
+		return repositoriesIds;
+	}
+}
