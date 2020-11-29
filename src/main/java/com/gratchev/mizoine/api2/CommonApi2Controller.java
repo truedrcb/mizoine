7ac8c28731022600575ef1da306591d984da1b29
Artem <artem@gratchev.com>
2020-11-29 22:33:04 +0100

    Sketch for API 2

--- src/main/java/com/gratchev/mizoine/api2/CommonApi2Controller.java
+++ src/main/java/com/gratchev/mizoine/api2/CommonApi2Controller.java
@@ -11,15 +11,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.gratchev.mizoine.api.ProjectApiController.ProjectInfo;
-
 import io.swagger.v3.oas.annotations.Operation;
 
 @Controller
 @RequestMapping("/api2")
 public class CommonApi2Controller {
 	private static final Logger log = LoggerFactory.getLogger(CommonApi2Controller.class);
-	
+
 	@Value("${config:mizoine-config.json}")
 	protected String configPath;
 
@@ -35,11 +33,47 @@ public class CommonApi2Controller {
 		appInfo.config = configPath;
 		return appInfo;
 	}
+	
+	public static class DescriptionDto {
+		public String fileName;
+		public String md;
+		public String text;
+		public String html;
+	}
+
+	public static class ProjectDto {
+		public String repositoryId;
+		public String projectId;
+		
+		public List<DescriptionDto> texts;
+	}
 
 	@Operation(summary = "Retrieve projects list")
 	@GetMapping("/repositories({repositoryId})/projects")
 	@ResponseBody
-	public List<ProjectInfo> getProjects(@PathVariable final String repositoryId) {
+	public List<ProjectDto> getProjects(@PathVariable final String repositoryId) {
 		return List.of();
+	}
+
+	@Operation(summary = "Retrieve project")
+	@GetMapping("/repositories({repositoryId})/projects({projectId})")
+	@ResponseBody
+	public ProjectDto getProject(@PathVariable final String repositoryId, @PathVariable final String projectId) {
+		return null;
+	}
+
+	public static class MentDto {
+		public String repositoryId;
+		public String projectId;
+		public String issueId;
+		public String mentId;
+	}
+
+	@Operation(summary = "Retrieve comment or attachment")
+	@GetMapping("/repositories({repositoryId})/projects({projectId})/issues({issueId})/ments({mentId})")
+	@ResponseBody
+	public MentDto getMent(@PathVariable final String repositoryId, @PathVariable final String projectId,
+			@PathVariable final String issueId, @PathVariable final String mentId) {
+		return null;
 	}
 }
