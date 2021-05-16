fe7816e992f10d8bb22b0999bf36dfbc5811818b
Artem <artem@gratchev.com>
2021-05-16 22:54:34 +0200

    Date deserializer test

--- src/main/java/com/gratchev/mizoine/repository/meta/LooseDateDeserializer.java
+++ src/main/java/com/gratchev/mizoine/repository/meta/LooseDateDeserializer.java
@@ -3,33 +3,27 @@ package com.gratchev.mizoine.repository.meta;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 
 public class LooseDateDeserializer extends JsonDeserializer<Date> {
-	private List<SimpleDateFormat> formatters = new ArrayList<>();
-
-	public LooseDateDeserializer() {
-		formatters.add(new SimpleDateFormat("dd-MM-yyyy HH:mm:ssZ"));
-		formatters.add(new SimpleDateFormat("dd-MM-yyyy HH:mm:ss"));
-		formatters.add(new SimpleDateFormat("dd-MM-yyyy HH:mm"));
-		formatters.add(new SimpleDateFormat("dd-MM-yyyy"));
-		formatters.add(new SimpleDateFormat("dd/MM/yyyy HH:mm:ssZ"));
-		formatters.add(new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"));
-		formatters.add(new SimpleDateFormat("dd/MM/yyyy HH:mm"));
-		formatters.add(new SimpleDateFormat("dd/MM/yyyy"));
-	}
+	private final List<SimpleDateFormat> formatters = List.of(
+		new SimpleDateFormat("dd-MM-yyyy HH:mm:ssZ"),
+		new SimpleDateFormat("dd-MM-yyyy HH:mm:ss"),
+		new SimpleDateFormat("dd-MM-yyyy HH:mm"),
+		new SimpleDateFormat("dd-MM-yyyy"),
+		new SimpleDateFormat("dd/MM/yyyy HH:mm:ssZ"),
+		new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"),
+		new SimpleDateFormat("dd/MM/yyyy HH:mm"),
+		new SimpleDateFormat("dd/MM/yyyy"));
 
 	@Override
-	public Date deserialize(final JsonParser parser, 
-			final DeserializationContext ctxt) throws IOException, JsonProcessingException {
+	public Date deserialize(final JsonParser parser, final DeserializationContext c) throws IOException {
 		final String text = parser.getText();
 		for (final SimpleDateFormat formatter : formatters) {
 			try {
