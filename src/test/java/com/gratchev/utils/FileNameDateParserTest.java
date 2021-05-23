0e74e1d4c14caef7f3984bfcf6e448ae9b73afc7
Artem <artem@gratchev.com>
2021-05-23 15:05:41 +0200

    Extract creation date from file name: Template place holders

--- src/test/java/com/gratchev/utils/FileNameDateParserTest.java
+++ src/test/java/com/gratchev/utils/FileNameDateParserTest.java
@@ -1,6 +1,5 @@
 package com.gratchev.utils;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
@@ -14,7 +13,7 @@ public class FileNameDateParserTest {
 		final FileNameDateParser parser = new FileNameDateParser();
 		// https://stackoverflow.com/questions/415580/regex-named-groups-in-java
 		// https://docs.oracle.com/javase/tutorial/i18n/format/simpleDateFormat.html
-		parser.addTemplate(".*(?<yyyy>(20|19)..)_(?<MM>..)_(?<dd>..).*");
+		parser.addTemplate(".*{yyyy}_{MM}_{dd}.*");
 		Date d = parser.parse("Kontoauszug_1234567890_Nr_2021_001_per_2021_01_04.pdf");
 		assertThat(d).hasYear(2021).hasMonth(1).hasDayOfMonth(4);
 	}
@@ -22,8 +21,8 @@ public class FileNameDateParserTest {
 	@Test
 	void extractMultipleDateFormats() throws ParseException {
 		final FileNameDateParser parser = new FileNameDateParser();
-		parser.addTemplate(".*(?<yyyy>(20|19)..)_(?<MM>\\d\\d)_(?<dd>..).*");
-		parser.addTemplate(".*_(?<dd>..)\\.(?<MM>..)\\.(?<yyyy>....)_.*");
+		parser.addTemplate(".*{yyyy}_{MM}_{dd}.*");
+		parser.addTemplate(".*_{dd}\\.{MM}\\.{yyyy}_.*");
 		assertThat(parser.parse("Depotauszug_vom_01.04.2021_zu_Depot_1234567_-_202104020987JD65.pdf")).hasYear(2021).hasMonth(4).hasDayOfMonth(1);
 		assertThat(parser.parse("Kreditkartenabrechnung_1234xxxxxxxx5678_per_2021_01_22.pdf")).hasYear(2021).hasMonth(1).hasDayOfMonth(22);
 	}
